ae7abb1190e0132a8880ffa107145099bc2e750e
Manjindersingh8670 <Manjindersingh8670@conestogac.on.ca>
2020-04-19 23:41:41 -0400

    setting up the project

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
